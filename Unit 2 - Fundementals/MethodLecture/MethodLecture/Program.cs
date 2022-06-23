908baaa7540aa06e211018a332d9c20aa42b1939
Jeo <jeo961@gmail.com>
2022-06-23 15:26:07 -0400

    methods

---
+++ Unit 2 - Fundementals/MethodLecture/MethodLecture/Program.cs
@@ -0,0 +1,144 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string hUser = returnHelloUser("Justin");
+Console.WriteLine(hUser);
+Console.WriteLine(returnHelloUser("David"));
+GreetUser();
+CanVote(24);
+CanVote(17);
+Console.WriteLine(getTaxRate());
+
+double total = 150 + (150 * getTaxRate());
+Console.WriteLine($"Grand Total: {total}");
+
+Console.WriteLine(adder(2,9));
+
+//#1
+Console.WriteLine(convertMinuteToSeconds(20));
+//#2
+Console.WriteLine(areaTriangle(10,5));
+//#3
+Console.WriteLine(convertNegative(3));
+Console.WriteLine(convertNegative(-3));
+//#4
+Console.WriteLine(convertPositive(-5));
+Console.WriteLine(convertPositive(5));
+//#5
+greetUser();
+
+string value = "Jjdfks";
+string returnvalue = returnHelloUser(value);
+Console.WriteLine(value);
+Console.WriteLine(returnvalue);
+
+static string returnHelloUser(string name)
+{
+    name = name.ToLower();
+    string result = $"Hello, {name}.";
+    return result;
+}
+
+static void GreetUser()
+{
+    Console.WriteLine("Hello and welcome to Method Lecture");
+}
+
+//overloaded
+//static void GreetUser(string name)
+//{
+
+//}
+
+static void CanVote(int age)
+{
+    if(age >= 18)
+    {
+        Console.WriteLine("Can vote");
+    }
+    else
+    {
+        Console.WriteLine("Cannot vote");
+    }
+}
+
+static double getTaxRate()
+{
+    return 0.06;
+}
+
+static double adder(double x, double y)
+{
+    return x + y;
+}
+
+//exercise problems
+//1
+static int convertMinuteToSeconds(int minutes)
+{
+    return minutes * 60;
+}
+
+//2
+static double areaTriangle(double height, double length)
+{
+    return .5 * length * height;
+}
+
+//3
+static int convertNegative(int x)
+{
+    if(x > 0)
+    {
+        return -x;
+    }
+    else
+    {
+        return x;
+    }
+
+}
+
+//4
+static int convertPositive(int x)
+{
+    if(x < 0)
+    {
+        return x * -1;
+    }
+    else
+    {
+        return x;
+    }
+}
+
+//5
+static void greetUser()
+{
+    Console.WriteLine("Please enter your name");
+    string name = Console.ReadLine();
+    Console.WriteLine($"Hello {name}");
+}
+
+//Pass in by reference
+int currentage = 24;
+string myNickname = ager(ref currentage);
+Console.WriteLine(currentage);
+Console.WriteLine(myNickname);
+
+static string ager(ref int age)
+{
+    age = 75;
+    return "Old man";
+}
+
+//Random
+Console.WriteLine(GetRandom());
+Console.WriteLine(GetRandom());
+Console.WriteLine(GetRandom());
+Console.WriteLine(GetRandom());
+
+static int GetRandom()
+{
+    Random rnd = new Random();
+    return rnd.Next(1,7);
+}
---
+++ Unit 2 - Fundementals/NumberAnalyzer/NumberAnalyzer/Program.cs
@@ -0,0 +1,60 @@
+﻿Console.WriteLine("Welcome to the Number Analyzer.");
+
+Console.WriteLine("What is your name?");
+string name = Console.ReadLine();
+
+bool runProgram = true;
+
+while (runProgram)
+{
+    //create choice before loop to use it outside of loop
+    int choice = 0;
+    //while true is an infinite loop
+    while (true)
+    {
+        Console.WriteLine(name + ", please enter a number between 1-100");
+        choice = int.Parse(Console.ReadLine());
+        if (choice >= 1 && choice <= 100)
+        {
+            //if condition is met, this breaks out of infinite loop
+            break;
+        }
+    }
+
+
+    //Analyzing
+    //odd
+    if (choice < 60 && choice % 2 == 1)
+    {
+        Console.WriteLine(name + " your answer is:" + choice + " Odd and less than 60");
+    }
+    else if (choice > 60 && choice % 2 == 1)
+    {
+        Console.WriteLine(name + " your answer is:" + choice + " Odd and greater than 60");
+    }
+    //even
+    else if (choice >= 2 && choice <= 24)
+    {
+        Console.WriteLine(name + " your answer is:" + "Even and less than 25");
+    }
+    else if (choice >= 26 && choice <= 60)
+    {
+        Console.WriteLine(name + " your answer is:" + "Even and between 26 and 60 inclusive.");
+    }
+    else if (choice > 60)
+    {
+        Console.WriteLine(name + " your answer is:" + choice + " Even and greater than 60");
+    }
+
+    //get out of loop
+    Console.WriteLine("Would you like to continue? y/n");
+    string loopChoice = Console.ReadLine();
+    if (loopChoice.ToLower().Trim() == "y")
+    {
+        //doesn't do anything yet
+    }
+    else if (loopChoice.ToLower().Trim() == "n")
+    {
+        runProgram = false;
+    }
+}
