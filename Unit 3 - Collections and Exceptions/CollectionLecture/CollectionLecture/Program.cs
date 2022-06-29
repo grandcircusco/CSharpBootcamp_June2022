89a30feeaea8fbed8c90c6ac73fc28f91fd3c811
Jeo <jeo961@gmail.com>
2022-06-29 14:29:17 -0400

    collections

---
+++ Unit 3 - Collections and Exceptions/CollectionLecture/CollectionLecture/Program.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections;
+
+//normal list
+List<string> names = new List<string>();
+names.Add("Justin");
+//not allowed
+//names.Add(1);
+foreach(string n in names)
+{
+    Console.WriteLine(n);
+}
+
+//Alternet list creating
+List<string> staff = new List<string>()
+{
+    "Billy",
+    "Damien",
+    "Justin",
+    "Josh"
+};
+Console.WriteLine(staff[1]);
+Console.WriteLine(staff.Count);
+//List Methods
+staff.Insert(2, "Ali");
+foreach(string s in staff)
+{
+    Console.WriteLine(s);
+}
+//staff.RemoveAt(2);
+staff.Remove("Ali");
+foreach (string s in staff)
+{
+    Console.WriteLine(s);
+}
+
+//Dictionaries
+Dictionary<string, bool> tasty = new Dictionary<string, bool>();
+tasty.Add("Chicken Curry", true);
+tasty.Add("Asparagus", false);
+Console.WriteLine(tasty["Asparagus"]);
+
+//Dictionary foreach
+//KeyValuePair must match Dictionary datatypes
+foreach (KeyValuePair<string, bool> kvp in tasty)
+{
+    //kvp.Key returns key
+    //kvp.Value returns value
+    if(kvp.Value == true)
+    {
+        Console.WriteLine($"{kvp.Key} is tasty");
+    }
+    else
+    {
+        Console.WriteLine($"{kvp.Key} is icky!");
+    }
+}
+
+Dictionary<string, int> Phonebook = new Dictionary<string, int>();
+Phonebook.Add("Yumi's Pizza", 1234567890);
+Phonebook.Add("Young Chad's Deli", 1112223333);
+Phonebook.Add("Paco's Tacos", 1000000001);
+string foodChoice = "Yumi's Pizza";
+Console.WriteLine(Phonebook[foodChoice]);
+Console.WriteLine("Phonebook:");
+foreach(KeyValuePair<string, int> kvp in Phonebook)
+{
+    Console.WriteLine($"{kvp.Key}: {kvp.Value}");
+}
+
+//Exercise 47
+List<string> prompt = new List<string>();
+bool runExercise = true;
+while(runExercise == true)
+{
+    Console.WriteLine("Enter some text");
+    prompt.Add(Console.ReadLine());
+
+    foreach (string s in prompt)
+    {
+        Console.Write(s + " ");
+    }
+    Console.WriteLine();
+
+    while (true)
+    {
+        Console.WriteLine("Would you like to continue? y/n");
+        string choice = Console.ReadLine();
+        if (choice == "y")
+        {
+            break;
+        }
+        else if (choice == "n")
+        {
+            runExercise = false;
+            break;
+        }
+    }
+    
+}
+//exercise 48
+List<int> NumbersToAdd = new List<int>();
+bool addNumbers = true;
+while (addNumbers)
+{
+    Console.WriteLine("Enter a number. q to quit");
+    string numChoice = Console.ReadLine();
+    if(numChoice == "q")
+    {
+        break;//Stop adding nums
+    }
+    else
+    {
+        NumbersToAdd.Add(int.Parse(numChoice));
+    }
+}
+string result = string.Join("+", NumbersToAdd);
+Console.WriteLine($"{result} = {NumbersToAdd.Sum()}");
+
+//---------------------------------------------------------------------------
+//Non-Generic
+//DO NOT USE IN MODERN CODE. OUTDATED. JUSTIN WILL FIGHT YOU (ง'̀-'́)ง
+
+
+//Array List
+ArrayList numbers = new ArrayList();
+numbers.Add(5);
+//Nongenerics allow any data
+//numbers.Add("Michigan");
+foreach(int i in numbers)
+{
+    Console.WriteLine(i);
+}
+
+//Hashtable
+Hashtable ht = new Hashtable();
+ht.Add("001", "John");
+ht.Add("002", "Paul");
+Console.WriteLine(ht["001"]);
+//Nongenerics allow any data
+//ht.Add(3, "Steve");
+//Console.WriteLine(ht["003"]);
---
+++ Unit 3 - Collections and Exceptions/LinqLecture/LinqLecture/Program.cs
@@ -0,0 +1,55 @@
+﻿//Lamdba Expressions
+List<int> numbers = new List<int>
+{
+    5,4,1,3,9,8,6,7,2,0
+};
+//lamdba
+//unicorn => unicorn % 2 == 1
+int oddNumbers = numbers.Count(unicorn => unicorn % 2 == 1);
+
+Console.WriteLine(oddNumbers);
+
+//Query Expression Syntax
+List<int> FiveOrGreater = (from n in numbers
+                      where n >= 5
+                      select n).ToList();
+foreach(int n in FiveOrGreater)
+{
+    Console.WriteLine(n);
+}
+
+//Method Syntax
+List<int> FiveOrGreater2 = numbers.Where(n => n >= 5).ToList();
+foreach(int n in FiveOrGreater2)
+{
+    Console.WriteLine(n);
+}
+
+//Methods
+
+//Where
+//Its a filter. Returns collection based on condition
+List<int> Ages = new List<int>() { 1, 19, 15, 21, 45, 50 };
+List<int> LegalAges = Ages.Where(n => n >= 21).ToList();
+
+//Foreach
+//Quickly create a foreach on a loop
+LegalAges.ForEach(a => Console.WriteLine(a));
+
+//First (There is a Last as well)
+//Return the first matching element
+int Child = Ages.First(a => a < 5);
+Console.WriteLine(Child);
+
+//Any
+//Returns a true if at least 1 element matches condition
+bool isOverFiftyCheck = Ages.Any(a => a >= 50);
+Console.WriteLine(isOverFiftyCheck);
+
+//Average
+double avgAge = Ages.Where(a => a >= 21).Average();
+Console.WriteLine(avgAge);
+
+//Sum
+double sumAge = Ages.Where(a => a < 21).Sum();
+Console.WriteLine(sumAge);
