e3ee4478110a8a7bc52339167258fa62ca921242
Jeo <jeo961@gmail.com>
2022-07-21 14:24:50 -0400

    ASP TIME

---
+++ Unit 7 MVC APIs/ASP Lecture/ASP Lecture/Controllers/HomeController.cs
@@ -0,0 +1,75 @@
+﻿using ASP_Lecture.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ASP_Lecture.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            Cat c = new Cat()
+            {
+                Name = "Yumi",
+                Description = "The largest problem."
+            };
+            return View(c);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Greeting()
+        {
+            //ViewBag and ViewData basically interchangable
+            ViewBag.Project = "ASP Project";
+            return View();
+        }
+
+        public IActionResult Info()
+        {
+            //ViewBag and ViewData basically interchangable
+            ViewData["Name"] = "Justin";
+            ViewData["Age"] = 25;
+            return View();
+        }
+
+        public IActionResult CatForm()
+        {
+            return View();
+        }
+
+        //Any value sent through form must be caught by parameters
+        //public IActionResult CatResult(string Name, string Description)
+        //{
+        //    Cat c = new Cat(Name, Description);
+        //    return View(c);
+        //}
+
+        //if form values match an object, you can just take in an object
+        public IActionResult CatResult(Cat c)
+        {
+            if(c.Name == "a")
+            {
+                return RedirectToAction("CatForm");
+            }
+            return View(c);
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Unit 7 MVC APIs/ASP Lecture/ASP Lecture/Models/Cat.cs
@@ -0,0 +1,10 @@
+﻿namespace ASP_Lecture.Models
+{
+    public class Cat
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        //ASP does not like constructors
+    }
+}
